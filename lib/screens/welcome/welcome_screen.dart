50aff6d1af3be54716d50e54ae693cf60bc46b82
tomohiro1024 <gongyezhihong732@gmail.com>
2022-04-16 21:33:26 +0900

    welcome_screen画像配置

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -5,6 +5,38 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://cdn.pixabay.com/photo/2021/09/02/10/36/lighthouse-6593162_1280.jpg'),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          SafeArea(
+              child: Column(
+            children: [
+              Spacer(),
+              Center(
+                child: Text(
+                  'クイズを始めよう！',
+                  style: Theme.of(context).textTheme.headline4?.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Text(
+                "名前を入力してください",
+                style: TextStyle(color: Colors.white),
+              ),
+              Spacer(),
+            ],
+          ))
+        ],
+      ),
+    );
   }
 }
